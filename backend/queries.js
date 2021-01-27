d393099b5ac2330203f515c032281ec532bef181
dimoskokkonos <77458621+dimoskokkonos@users.noreply.github.com>
2021-01-27 10:27:32 +0200

    Update queries.js

--- backend/queries.js
+++ backend/queries.js
@@ -4,7 +4,7 @@ const pool = new Pool({
     user: 'postgres', 
     host: 'localhost', 
     database: 'backend', 
-    password: 'waitforit20', 
+    password: 'temp', 
     dialect: 'postgres', 
     port: 5432,
     max: 30
